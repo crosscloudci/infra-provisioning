80b92db3445a0b6e3d8998c03192c66f14850822
denverwilliams <denver@debian.nz>
2019-06-12 14:46:30 -0400

    Split master/worker nodes into independent blocks

--- terraform/packet.tf
+++ terraform/packet.tf
@@ -1,9 +1,20 @@
-resource "packet_device" "cncf" {
-  count            = "${ var.packet_node_count }"
+resource "packet_device" "master" {
+  count            = "${ var.packet_master_node_count }"
   hostname         = "${ var.name }${ count.index + 1 }"
   facility         = "${ var.packet_facility }"
   project_id       = "${ var.packet_project_id }"
   plan             = "${ var.packet_master_device_plan }" # Packet.net machine type
+  billing_cycle    = "${ var.packet_billing_cycle }" # hourly or monthly
+  operating_system = "${ var.packet_operating_system }"
+ 
+}
+
+resource "packet_device" "worker" {
+  count            = "${ var.packet_worker_node_count }"
+  hostname         = "${ var.name }${ count.index + 1 }"
+  facility         = "${ var.packet_facility }"
+  project_id       = "${ var.packet_project_id }"
+  plan             = "${ var.packet_worker_device_plan }" # Packet.net machine type
   billing_cycle    = "${ var.packet_billing_cycle }" # hourly or monthly
   operating_system = "${ var.packet_operating_system }"
  
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,18 @@
 provider "packet" {
+
   auth_token = "${ var.packet_api_key }"
 }
-variable "name" { default = "pktgen" }
-variable "packet_node_count" { default = "1" }
-variable "packet_project_id" {}
+
 variable "packet_api_key" {}
+variable "packet_project_id" {}
+
+variable "name" { default = "pktgen" }
+variable "packet_master_node_count" { default = "1" }
+variable "packet_worker_node_count" { default = "1" }
 variable "packet_facility" { default = "sjc1" }
 variable "packet_billing_cycle" { default = "hourly" }
 
 # VM Image and size
 variable "packet_master_device_plan" { default = "m2.xlarge.x86" }
+variable "packet_worker_device_plan" { default = "m2.xlarge.x86" }
 variable "packet_operating_system" { default = "ubuntu_16_04" }
