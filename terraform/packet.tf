8e5392c7be0cf0e2e2a50e5cda07475a28edac4b
denverwilliams <denver@debian.nz>
2019-06-13 20:10:42 -0400

    Fix facilities argument

--- terraform/packet.tf
+++ terraform/packet.tf
@@ -1,7 +1,7 @@
 resource "packet_device" "master" {
   count            = "${ var.packet_master_node_count }"
   hostname         = "${ var.name }${ count.index + 1 }"
-  facility         = "${ var.packet_facility }"
+  facilities       = "${ var.packet_facility }"
   project_id       = "${ var.packet_project_id }"
   plan             = "${ var.packet_master_device_plan }" # Packet.net machine type
   billing_cycle    = "${ var.packet_billing_cycle }" # hourly or monthly
@@ -12,7 +12,7 @@ resource "packet_device" "master" {
 resource "packet_device" "worker" {
   count            = "${ var.packet_worker_node_count }"
   hostname         = "${ var.name }${ count.index + 1 }"
-  facility         = "${ var.packet_facility }"
+  facilities       = "${ var.packet_facility }"
   project_id       = "${ var.packet_project_id }"
   plan             = "${ var.packet_worker_device_plan }" # Packet.net machine type
   billing_cycle    = "${ var.packet_billing_cycle }" # hourly or monthly
