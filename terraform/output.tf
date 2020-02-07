bfe8b6d1b388112728b84ab659eec18d00146146
denverwilliams <denver@debian.nz>
2020-02-07 17:00:48 -0500

    Output node data for node_group_three cncf/cnf-testbed#330

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,5 @@
 output "node_group_one" { value = "${ join(" ", packet_device.node_group_one.*.access_public_ipv4) }" }
 
 output "node_group_two" { value = "${ join(" ", packet_device.node_group_two.*.access_public_ipv4) }" }
+
+output "node_group_three" { value = "${ join(" ", packet_device.node_group_three.*.access_public_ipv4) }" }
